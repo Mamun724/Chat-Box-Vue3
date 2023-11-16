1beeb54cdc175270f75696eea3a8e678ace6428b
Md. Abdulla Al Mamun <abdulla.mamun@enosisbd.com>
2023-11-16 12:49:29 +0600

    Fix sender-receiver avatar by using v-img in v-avatar for vuetify3

--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -4,7 +4,7 @@
       <v-col cols="10">
         <div class="d-flex align-start">
           <v-avatar size="36" class="mr-2 avatar">
-            <img :src="receiver.profilePicture" :alt="receiver.username">
+            <v-img :src="receiver.profilePicture" :alt="receiver.username"/>
           </v-avatar>
           <div class="message-content" :title="messageTime">
             {{ message.content }}
@@ -20,7 +20,7 @@
             {{ message.content }}
           </div>
           <v-avatar size="36" class="ml-2 avatar">
-            <img :src="sender.profilePicture" :alt="sender.username">
+            <v-img :src="sender.profilePicture" :alt="sender.username"/>
           </v-avatar>
         </div>
       </v-col>
